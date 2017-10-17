9d5073e56c49079debac7843bfd7eb86b6d444af
yongxuanluo <32734864+yongxuanluo@users.noreply.github.com>
2017-10-17 15:45:55 +1100

    Update upload_result_azure.py

--- Speech_to_Text/upload_result_azure.py
+++ Speech_to_Text/upload_result_azure.py
@@ -3,13 +3,19 @@ from azure.storage.file import FileService
 from azure.storage.file import ContentSettings
 import os
 
+# Set up
 directory = os.fsencode('C:/Users/luopa/Desktop/Cognitive/result')
 file_service = FileService(account_name='eyc3blob', account_key='8lWwXYYTbRtpg02HRd/DiVrSfyF4xNqtGmUzIy1GkLLOG1tXsXGLZG2KbDCz3XerLM1xPV4nl62YgmhAYAAUTQ==')
+
+# Create result directory
+# file_service.create_directory(share_name='eyc3file/speech', directory_name='result')
+
+# Looping through the documents
 generator = file_service.list_directories_and_files('eyc3file/speech')
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     print(filename)
     file_service.create_file_from_path(share_name='eyc3file/speech',
-                                  directory_name=None,
+                                  directory_name='result',
                                   file_name=filename,
                                   local_file_path='C:/Users/luopa/Desktop/Cognitive/result'+'/'+filename)
